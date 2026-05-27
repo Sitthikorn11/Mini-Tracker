0f39af5617ff2ce9dfd868ea646e65dd05157b98
Sitthikorn11 <zervew111@gmail.com>
2026-05-27 17:29:39 +0700

    Fix mass assignment bug for category icons

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['user_id', 'name', 'type', 'color'];
+    protected $fillable = ['user_id', 'name', 'type', 'color', 'icon'];
 
     public function user()
     {
